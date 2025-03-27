ad6e24d756c6b64102644b7677a8da7cec5715e4
Xaya <contact.xaya@gmail.com>
2025-03-27 20:45:58 +0100

    Initial commit

---
+++ src/main/kotlin/fr/xania/legendsClaims/LegendsClaims.kt
@@ -0,0 +1,14 @@
+package fr.xania.legendsClaims
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class LegendsClaims : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
